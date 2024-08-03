3e16a3e1eb6cba810b65d238b727bd3571ffab5f
mubarak96-max <mubarakmmm5@gmail.com>
2024-08-03 13:09:21 +0400

    third push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./components/Main";
 import QRCodePage from "./components/QRCode";
